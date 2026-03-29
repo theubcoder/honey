e0b2a1d812eab9b3e81a9a7bcca6216f9cfb61c4
ubaidullah127 <ubaid.ullah@dmarclytics.co.uk>
2026-03-29 22:42:23 +0500

    Clear all files

--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  turbopack: {},
-};
-
-export default nextConfig;
--- src/app/wishlist/loading.js
+++
@@ -1,9 +0,0 @@
-import { LoaderCircle } from "lucide-react";
-
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center min-h-[60vh]">
-      <LoaderCircle size={48} className="animate-spin text-primary" />
-    </div>
-  );
-}
